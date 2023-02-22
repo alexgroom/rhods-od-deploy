19c497b085eb600826b20fa935481a21e0c186df
Alex Groom <alex.groom@gmail.com>
2023-02-22 18:13:25 +0000

    updates

--- htpasswd/updatehtpasswd.sh
+++ htpasswd/updatehtpasswd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 oc get secret htpasswd-secret -ojsonpath={.data.htpasswd} -n openshift-config | base64 --decode > users.htpasswd
-curl https://raw.githubusercontent.com/alexgroom/rhods-od-deploy/htpasswd/htpasswd >> users.htpasswd
+curl https://raw.githubusercontent.com/alexgroom/rhods-od-deploy/main/htpasswd/htpasswd >> users.htpasswd
 oc create secret generic htpasswd-secret --from-file=htpasswd=users.htpasswd --dry-run=client -o yaml -n openshift-config | oc replace -f -
 oc adm policy add-cluster-role-to-user cluster-admin opentlc-mgr
 rm users.htpasswd
